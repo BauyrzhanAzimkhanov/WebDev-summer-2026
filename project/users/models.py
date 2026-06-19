4926440772e516483dbf6c43a68542dae206ac7f
BauyrzhanAzimkhanov <azimkhanov4@gmail.com>
2026-06-19 13:23:47 +0500

    feat: added teacher model

--- project/users/models.py
+++ project/users/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Student(models.Model):
 
-    # def __str__(self):
-    #     return self.student_id + ", " + self.first_name + " " + self.last_name
+    def __str__(self):
+        return self.student_id + ", " + self.first_name + " " + self.last_name
     
     def get_account_balance(self):
         return self.account_balance + " tenge"
@@ -15,3 +15,15 @@ class Student(models.Model):
     last_name = models.CharField(max_length=20)
     faculty = models.CharField()
     account_balance = models.DecimalField(max_digits=20, decimal_places=6)
+
+class Teacher(models.Model):
+    employee_id = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=20)
+    second_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    faculty = models.CharField()
+    salary = models.DecimalField(max_digits=20, decimal_places=6)
+
+    def __str__(self):
+        return self.employee_id + ", " + self.first_name + " " + self.last_name
+    
